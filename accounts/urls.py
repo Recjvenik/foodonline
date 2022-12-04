8dfc5be8a6cd9df2763cc2f732e50ffb85c0e39b
Recjvenik <himanshum9600@gmail.comm>
2022-12-04 19:43:26 +0530

    vendor registor

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.registerUser, name='register-user')
+    path('register/', views.registerUser, name='register-user'),
+    path('register-vendor/', views.registerVendor, name='register-vendor'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserForm
-from .models import User
+from vendor.forms import VendorForm
+from .models import User, UserProfile
 from django.contrib import messages
+from django.template.defaultfilters import slugify
 
 # Create your views here.
 def registerUser(request):
@@ -30,3 +32,36 @@ def registerUser(request):
             
     context['form'] = form
     return render(request, 'accounts/register.html', context)
+
+
+def registerVendor(request):
+    context = dict()
+    form = UserForm(request.POST or None)
+    v_form = VendorForm(request.POST, request.FILES or None)
+
+    if request.method == 'POST':
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            vendor_name = v_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name)+'-'+str(user.id)
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, 'Your account has been created successfully...!!')
+            return redirect('register-vendor')
+        else:
+            print('invalid form')
+            print(form.errors)
+            print(v_form.errors)
+    context['form'] = form
+    context['v_form'] = v_form
+    return render(request, 'accounts/register-vendor.html', context)
